6d270d7c9d254e6a9f141990b8fa33f421646c72
Odin Dutton <odindutton@gmail.com>
2020-01-23 09:26:36 +1100

    Parse Participant Invited body

--- examples/events/participant.rs
+++ examples/events/participant.rs
@@ -1,13 +1,26 @@
+use serde::Deserialize;
+use uuid::Uuid;
+
 #[derive(Debug)]
 pub enum Participant {
-    Invited,
+    Invited(InvitedBody),
     UnknownEvent,
 }
 
+#[derive(Debug, Deserialize)]
+pub struct InvitedBody {
+    survey_period_id: Uuid,
+    employee_id: Uuid,
+    invited_at: String,
+}
+
 impl Participant {
-    pub fn build(event_type: &str, _body: serde_json::Value) -> Self {
+    pub fn build(event_type: &str, body: serde_json::Value) -> Self {
+        dbg!(&body);
         match event_type {
-            "Invited" => Participant::Invited,
+            "Invited" => Participant::Invited(
+                serde_json::from_value(body).expect("unable to parse SurveyCreatedBody"),
+            ),
             _ => Participant::UnknownEvent,
         }
     }
