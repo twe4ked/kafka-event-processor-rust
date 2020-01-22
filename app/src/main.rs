20b435f53c641dc86b08fe3730edbd605af8fc8b
Odin Dutton <odindutton@gmail.com>
2020-01-23 09:43:35 +1100

    Split create/example into two sub crates, app and framework

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,7 +2,7 @@ mod events;
 
 struct Processor;
 
-impl event_processor::Processor<events::Event> for Processor {
+impl framework::Processor<events::Event> for Processor {
     fn process(&self, event: events::Event) {
         match &event.domain_event {
             events::DomainEvent::Survey(domain_event) => match domain_event {
@@ -29,5 +29,5 @@ fn main() {
     let topic = std::env::var("KAFKA_TOPIC").expect("KAFKA_TOPIC not provided");
     let processor = Processor {};
 
-    event_processor::run(broker, topic, processor);
+    framework::run(broker, topic, processor);
 }
